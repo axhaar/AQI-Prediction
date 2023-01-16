a6f0807748a525a8f2123117d44fe5ee615b26a2
axhaar <pranshumishra2001@gmail.com>
2023-01-17 00:45:32 +0530

    code for fetching data

---
+++ fetch.py
@@ -0,0 +1,27 @@
+import os
+import time
+import requests
+import sys
+
+def retrieve_html():
+    for year in range(2013,2023):
+        for month in range(1,13):
+            if(month<10):
+                url='http://en.tutiempo.net/climate/0{}-{}/ws-421820.html'.format(month,year)
+            else:
+                url='http://en.tutiempo.net/climate/{}-{}/ws-421820.html'.format(month,year)
+            texts=requests.get(url)
+            text_utf=texts.text.encode('utf=8')
+            
+            if not os.path.exists("D:/kinda sorta projects/Air Quality/v1/Data/Html_Data/{}".format(year)):
+                os.makedirs("D:/kinda sorta projects/Air Quality/v1/Data/Html_Data/{}".format(year))
+            with open("D:/kinda sorta projects/Air Quality/v1/Data/Html_Data/{}/{}.html".format(year,month),"wb") as output:
+                output.write(text_utf)
+                # D:/kinda sorta projects/Air Quality/v1/Data/Real-Data5
+            
+        sys.stdout.flush()
+
+if __name__=="__main__":
+    start_time=time.time()
+    retrieve_html()
+    stop_time=time.time()
